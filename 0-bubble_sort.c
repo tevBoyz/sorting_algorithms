f3a97faf4d4bdd15da11f43070688c717112e976
Dawit <dawit.ttamiru@gmail.com>
2022-08-16 11:49:40 -0700

    task 0, readme, and sort.h

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort algorithm func
+ * @array: array to sort
+ * @size: size of array
+ * Return: none
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+size_t i = 0, j = 0;
+int tmp = 0;
+
+if (array == NULL || size == 0)
+return;
+
+for (; i < size - 1; i++)
+{
+for (; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+tmp = array[j + 1];
+array[j + 1] = array[j];
+array[j] = tmp;
+print_array(array, size);
+}
+}
+}
+}
---
+++ sort.h
@@ -0,0 +1,31 @@
+#ifndef __SORT__
+#define __SORT__
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct listint_s - doubly linked list node
+ *
+ * @n: interger data
+ * @prev: pointer to prev node
+ * @next: pointer to next node
+ */
+typedef struct listint_s
+{
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
+} listint_t;
+
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+
+#endif /* __SORT__ */
